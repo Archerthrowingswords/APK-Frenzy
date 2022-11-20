638a1427fc6bebaeeffd23a4288d041b2db71e0b
keiiori <65444752+keiiori@users.noreply.github.com>
2022-11-20 14:53:45 +0800

    fixed calling jadx

--- main.py
+++ main.py
@@ -28,8 +28,9 @@ file = "./jadx/bin/schedule.txt"
 #         print(f"{line}")
 
 @app.command()
-def main(f: Path = typer.Option(None)):
+def main(f: Path = typer.Option(default=True, resolve_path=True,)):
     apkname = os.path.basename(f)
+    print(f)
     # if f is None:
     #     print("No config file")
     #     raise typer.Abort()
@@ -44,7 +45,13 @@ def main(f: Path = typer.Option(None)):
     if apkname.endswith(".apk"):
         print("------------------------------------------------------------------------------")
         print(f"is APK: {apkname}")
-        os.system(f"java - {jadx} -d out /{f}")
+        os.environ["PATH"] = f"{os.environ['PATH']};.\jadx\\bin\\"
+        # os.system(f"java -jar {jadx} -d out /{f}")
+        # os.system(f"{jadx} -d out /{f}")
+        # os.system(f"set PATH=%PATH%;D:\SP\Y3\FYP\FYP-Android-Bouncer\jadx\bin\\")
+        # os.system(f"path")
+        # print(f) not sure if you wanted me to leave in my work for your understanding but here you go
+        os.system(f"jadx -d out /{f}")
     else:
         for line in f:
           print(f"{line}")
