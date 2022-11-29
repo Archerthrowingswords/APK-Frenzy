e63a23167d2e5b412da39c4b62cff93a2ff4751a
AnonymousMilk <wong_hong_ngai@s2016.sst.edu.sg>
2022-11-29 11:45:48 +0800

    Added search for critical perms
    Looks through a list of critical permissions and finds if it exists

--- search_script.py
+++ search_script.py
@@ -14,6 +14,10 @@ tree = ET.parse(filename)
 root = tree.getroot()
 
 permissions_list = []
+found_crit_perms = []
+
+# just an example of critical permissions
+critical_permissions_list = ["android.permission.ACCESS_FINE_LOCATION", "android.permission.READ_EXTERNAL_STORAGE"]
 
 for permissions in root.iter('uses-permission'):
     # print(permissions.attrib['{http://schemas.android.com/apk/res/android}name'])
@@ -22,4 +26,8 @@ for permissions in root.iter('uses-permission'):
 # Remove duplicates from permissions_list
 permissions_list = list(dict.fromkeys(permissions_list))
     
-print(permissions_list)
+for i in range(len(permissions_list)):
+    if (permissions_list[i] in critical_permissions_list):
+        found_crit_perms.append(permissions_list[i])
+
+print(found_crit_perms)
