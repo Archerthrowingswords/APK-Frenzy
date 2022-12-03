4eb1313824db9d17e7ac6b8e7c5ea138999267e8
keiiori <65444752+keiiori@users.noreply.github.com>
2022-12-03 11:55:38 +0800

    Update java_extraction.py
    fixed an issue where only first keyword would be detected

--- java_extraction.py
+++ java_extraction.py
@@ -11,22 +11,30 @@ for file in out:
     if file.name.endswith(".java"): 
         log += file.__str__() + "\n"
         linecount = 0
+        keywordFound = False
 
         # javafile = file.name
         with open(file) as readfile:
             for keyword in keywords:
                 if file.read_text().find(keyword) != -1:
+                    keywordFound = True
+                else:
+                    keywordFound = False
+
+            if keywordFound:
+                for keyword in keywords:
                     for line in readfile:
                         linecount += 1
                         for keyword in keywords:
                             if line.find(keyword) != -1:
+                                print(keyword + " Found")
                                 log += "Line " + linecount.__str__() + " - " + line.lstrip() + "\n"
                                 break
-                else:
-                    log += "No keywords found\n"
-                    break
+            else:
+                log += "No keywords found\n"
         
         log += "-----------------------------------------------------------\n\n"
+
 
 with open("javaperms.txt", "w") as javapermslog:
     javapermslog.write(log)
