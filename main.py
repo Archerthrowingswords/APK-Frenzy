a6313c2b272c7ff903edf07e7667249af90d8354
Wong Hong Ngai <wonghongngai123@gmail.com>
2022-12-15 02:34:17 +0800

    added detectA11yAbuse func

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import xml.etree.ElementTree as ET
 
 app = typer.Typer()
 
+directory = 'out'
 
 def extractManifestPerms():
-    directory = 'out'
     #may be updated to be more efficient
     out = Path(directory).rglob('*')
     for file in out:
@@ -39,8 +39,6 @@ def extractManifestPerms():
             return permissions_list
 
 def extractJavaPerms(): 
-
-    directory = 'out'
 
     out = Path(directory).rglob('*')
 
@@ -77,6 +75,73 @@ def extractJavaPerms():
     with open("javaperms.txt", "w") as javapermslog:
         javapermslog.write(log)
 
+def detectAccessibilityUI():
+    out = Path(directory).rglob('*')
+
+    manifestKeywords = ["android.permission.BIND_ACCESSIBILITY_SERVICE", "android.accessibilityservice.AccessibilityService", "android.accessibilityservice", 'canRetrieveWindowContent="true"']
+    optionalManifestKeyword = ["accessibilityEventTypes", "setServiceInfo()"]
+    foundOptionalKeywords = []
+
+    javaKeywords = ["onAccessibilityEvent"]
+    javaOptionalKeywords = ['Intent("android.settings.ACCESSIBILITY_SETTINGS")']
+    javaFoundOptionalKeywords = []
+
+    # Searching through Manifest XML file
+    for file in out:
+        if file.name.endswith("Manifest.xml"):
+            with open(file) as manifestFile:
+                for line in manifestFile:
+                    for manifestKeyword in manifestKeywords:
+                        if line.find(manifestKeyword) != -1:
+                            manifestKeywords.remove(manifestKeyword)
+                    for manifestKeyword in optionalManifestKeyword:
+                        if line.find(manifestKeyword) != -1:
+                            foundOptionalKeywords.append(manifestKeyword)
+                            optionalManifestKeyword.remove(manifestKeyword)
+
+        elif file.name.endswith(".java"): 
+
+            # Searching through Java files
+
+            if file.name.endswith(".java"): 
+                keywordFound = False
+                optionalKeywordFound = False
+
+                with open(file) as readfile:
+                    for javaKeyword in javaKeywords:
+                        if file.read_text().find(javaKeyword) != -1:
+                            keywordFound = True
+                            break
+
+                    for javaKeyword in javaOptionalKeywords:
+                        if file.read_text().find(javaKeyword) != -1:
+                            optionalKeywordFound = True
+                            break
+                        
+                    if keywordFound:
+                            for line in readfile:
+                                for javaKeyword in javaKeywords:
+                                    if line.find(javaKeyword) != -1:
+                                        javaKeywords.remove(javaKeyword)
+                                        print(javaKeywords)
+                                        break
+
+                    if optionalKeywordFound:
+                            for line in readfile:
+                                for javaKeyword in javaOptionalKeywords:
+                                    if line.find(javaKeyword) != -1:
+                                        javaFoundOptionalKeywords.append(javaKeyword)
+                                        break
+
+    if (manifestKeywords == [] and javaKeywords == [] and (foundOptionalKeywords != [] or javaOptionalKeywords != [])):
+        print("Malicious, found optional keywords")
+        if (foundOptionalKeywords != []):
+            print(foundOptionalKeywords)
+        if (javaOptionalKeywords != []):
+            print(javaOptionalKeywords)
+    elif (manifestKeywords == [] and javaKeywords == []):
+        print("Malicious")
+
 @app.command()
 def main(f: Path = typer.Option(default=True, resolve_path=True,)):
     apkname = os.path.basename(f)
@@ -94,6 +159,7 @@ def main(f: Path = typer.Option(default=True, resolve_path=True,)):
             os.system(f"jadx -d out /{f}")
             extractManifestPerms()
             extractJavaPerms()
+            detectAccessibilityUI()
         else:
             text = f.read_text()
             print(f"File is not an apk: {text}")
