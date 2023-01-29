aca2086b9b22265497a5e610f46e6ddca7a61440
TingYuan575 <72486722+TingYuan575@users.noreply.github.com>
2023-01-30 01:18:25 +0800

    Update Output Minor

--- apkfrenzy.py
+++ apkfrenzy.py
@@ -229,7 +229,7 @@ def scanResult():
     if (detectedPatterns == {}):
         print("No Malicous Patterns Detected")
         return
-    print(f"Patterns Detected:")
+    print(f"Pattern(s) Detected:")
     for i in detectedPatterns: print(f"\n-{i} (+{detectedPatterns[i][1]}%)\n{detectedPatterns[i][0]}")
     if (validManifestFoundOptionalKeywords != []):
         print(f"\nInteresting Manifest Keywords Found:")
@@ -280,7 +280,7 @@ def scanAndRequests( f: Path = typer.Option(default=None, resolve_path=True)):
     scanReq()
     scanResult()
     reqResult()
-
+    
 @app.callback(invoke_without_command=True,context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
 def main(ctx: typer.Context, f: Path = typer.Option(default=None,resolve_path=True)):
     """
@@ -295,7 +295,6 @@ def main(ctx: typer.Context, f: Path = typer.Option(default=None,resolve_path=Tr
         checkDetected(detectionPatterns)
         simpleScanResult()
     else:
-        print("test")
         # if user passed path into callback pass it into global var
         if f != None: 
             callbackFilePath = f
